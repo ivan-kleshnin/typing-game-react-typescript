f8553e2532053943775f4d5ade2ca33e40fca01a
Ivan Kleshnin <ivan@paqmind.com>
2020-09-29 14:35:58 +0300

    Initial commit

---
+++ components/Board.js
@@ -0,0 +1,109 @@
+import * as R from "rambdax"
+import React from "react"
+import * as L from "../lib"
+import vocabulary from "../vocabulary.json"
+
+let allWords = R.shuffle(vocabulary)
+
+// LOGIC ===========================================================================================
+export let makeRandomWord = (seed, {xFn = null, yFn = null, xmin = 0, xmax = 100, ymin = 0, ymax = 100} = {}) => {
+  seed = seed ?? R.random(xmin, xmax)
+  return {
+    str: L.pickRandom(allWords),
+    x: xFn ? xFn(seed) : R.random(xmin, xmax),
+    y: yFn ? yFn(seed) : R.random(ymin, ymax),
+  }
+}
+
+export let makeRandomWords = (n, settings = {}) => {
+  return R.pipe(
+    () => R.range(0, n),
+    R.mapIndexed((_, i) => makeRandomWord(i, settings))
+  )()
+}
+
+export let hasInput = (input) => (words) => {
+  return Boolean(R.find(word => word.str == input, words))
+}
+
+export let dropInput = (input) => (words) => {
+  return R.filter(word => word.str != input, words)
+}
+
+export let moveWord = (word, {xmin = 1, xmax = 1, ymin = 1, ymax = 1} = {}) => {
+  return {
+    ...word,
+    x: R.clamp(0, 100, word.x + R.random(xmin, xmax)),
+    y: R.clamp(0, 100, word.y + R.random(ymin, ymax)),
+  }
+}
+
+// VIEW ============================================================================================
+export function BoardView({words}) {
+  return <>
+    <div className="board-wrapper">
+      <div className="board">
+        {words.map(({str, x, y}, i) => <span key={i} className="word" style={{
+          left: x + "%",
+          bottom: y + "%",
+        }}>{str}</span>)}
+      </div>
+    </div>
+    <style jsx>{`
+      .board-wrapper {
+        width: 640px;
+        height: 480px;
+        padding: 4rem 4rem 0 4rem;
+        background: #dcdcdc;
+      }
+      
+      .board {
+        position: relative;
+        height: 100%;
+        background: #dcdcff;
+      }
+      
+      .word {
+        text-align: center;
+        position: absolute;
+        background: #cd5c5c;
+        border-radius: 10px;
+        padding: 0 0.5rem;
+      }
+    `}</style>
+  </>
+}
+
+export function ScreenView({className, children}) {
+  return <>
+    <div className={`screen ${className}`}>
+      {children}
+    </div>
+    <style jsx>{`
+      .screen {
+        display: flex;
+        width: 640px;
+        height: 480px;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+      }
+      
+      :global(.screen h1) {
+        font-size: 3rem;
+      }
+      
+      .screen.green {
+        background: #a8db8f;
+      }
+      
+      .screen.gray {
+        background: #dcdcdc;
+      }
+      
+      .screen.red {
+        background: #db8f8f;
+      }
+    `}</style>
+  </>
+}
---
+++ components/Game.js
@@ -0,0 +1,239 @@
+import * as R from "rambdax"
+import React, {useDeferredValue, useEffect, useState} from "react"
+import * as L from "../lib"
+import * as Board from "./Board"
+
+let vocabulary = ["react", "angular", "jquery", "vue", "backbone", "svelte"]
+
+let Status = {
+  Stopped: "Stopped",
+  Running: "Running",
+  Won: "Won",
+  Lost: "Lost",
+  Paused: "Paused",
+}
+
+let startGame = (state) => ({
+  words: Board.makeRandomWords(5, {xFn: (i) => i * 20, ymin: 80, ymax: 100}),
+  input: "",
+  secondsLeft: 60,
+  status: Status.Running,
+})
+
+let dropInput = (input) => (state) => {
+  return {
+    ...state,
+    words: Board.dropInput(input)(state.words),
+    input: ""
+  }
+}
+
+let hasWinningCond = (state) => !state.secondsLeft || !state.words.length
+
+let hasLosingCond = (state) => R.find(word => !word.y, state.words)
+
+let setInput = (input) => (state) => ({...state, input})
+
+let setStatus = (status) => (state) => ({...state, status})
+
+let nextTick = (state) => {
+  return {
+    ...state,
+
+    // Win. Timer
+    secondsLeft: Math.max(state.secondsLeft - 1, 0),
+
+    // Gravity
+    words: R.map(word => Board.moveWord(word, {xmin: -2, xmax: 2, ymin: -4, ymax: -2}), state.words),
+  }
+}
+
+let addWord = (state) => {
+  return {
+    ...state,
+    words: [...state.words, Board.makeRandomWord(null, {ymin: 80, ymax: 100})],
+  }
+}
+
+export function App() {
+  let [state, setState] = useState({
+    ...startGame(),
+    status: Status.Stopped,
+  })
+
+  let {words, input, status, secondsLeft} = state
+
+  // INPUT HANDLING
+  function handleStartingClick(i) {
+    if (status != Status.Running) {
+      setState(startGame)
+    }
+  }
+
+  function handleKeyDown(event) {
+    if (status == Status.Running) {
+      if (event.keyCode === 27) {
+        setState(setStatus(Status.Paused))
+      } else if (event.keyCode == 13) {
+        setState(setInput(""))
+      } else if (event.code.startsWith("Key") || event.code.startsWith("Digit")) {
+        setState(state => {
+          return {
+            ...state,
+            input: state.input + event.key,
+          }
+        })
+      }
+    } else if (status == Status.Paused) {
+      if (event.keyCode === 27) {
+        setState(setStatus(Status.Running))
+      }
+    }
+  }
+
+  useEffect(_ => {
+    document.addEventListener("keydown", handleKeyDown)
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [status])
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setState(state => ({...state, input: ""}))
+    }, 1000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [input])
+
+  // WIN/LOSE MECHANICS
+  useEffect(_ => {
+    if (status == Status.Running) {
+      if (Board.hasInput(input)(words)) {
+        setState(dropInput(input))
+      }
+    }
+  }, [words, input])
+
+  useEffect(_ => {
+    if (status == Status.Running) {
+      if (hasWinningCond(state)) {
+        setState(setStatus(Status.Won))
+      } else if (hasLosingCond(state)) {
+        setState(setStatus(Status.Lost))
+      }
+    }
+  }, [state])
+
+  // TIMERS
+  useEffect(_ => {
+    let timer = null
+    if (status == Status.Running && !timer) {
+      timer = setInterval(() => {
+        setState(nextTick)
+      }, 1000)
+    }
+    return () => {
+      clearInterval(timer)
+    }
+  }, [status])
+
+  useEffect(_ => {
+    let timer = null
+    if (status == Status.Running && !timer) {
+      timer = setInterval(() => {
+        setState(addWord)
+      }, 2000)
+    }
+    return () => {
+      clearInterval(timer)
+    }
+  }, [status])
+
+  return <div onClick={handleStartingClick}>
+    <ScreenBoxView status={status} words={words}/>
+    <StatusLineView status={status} input={input} secondsLeft={secondsLeft}/>
+  </div>
+}
+
+function ScreenBoxView({input, status, words}) {
+  switch (status) {
+    case Status.Running:
+    case Status.Paused:
+      return <Board.BoardView words={words}/>
+
+    case Status.Stopped:
+      return <Board.ScreenView className="gray">
+        <div>
+          <h1>Typing Game</h1>
+          <p className="small" style={{textAlign:"center"}}>Click anywhere to start!</p>
+        </div>
+      </Board.ScreenView>
+
+    case Status.Won:
+      return <Board.ScreenView className="green">
+        <div>
+          <h1>Victory!</h1>
+          <p className="small" style={{textAlign:"center"}}>Click anywhere to try again!</p>
+        </div>
+      </Board.ScreenView>
+
+    case Status.Lost:
+      return <Board.ScreenView className="red">
+        <div>
+          <h1>Defeat!</h1>
+          <p className="small" style={{textAlign:"center"}}>Click anywhere to try again!</p>
+        </div>
+      </Board.ScreenView>
+  }
+}
+
+function StatusLineView({status, input, secondsLeft}) {
+  function LeftSide({status, input}) {
+    return <div>
+      {
+        (_ => {
+           switch (status) {
+            case Status.Running: return (input ? input : "|")
+            case Status.Paused:  return "..."
+            default:             return "Lets Go!"
+          }
+        })()
+      }
+    </div>
+  }
+
+  function RightSide({status, secondsLeft}) {
+    return <>
+      {
+        (_ => {
+          switch (status) {
+            case Status.Running: return `Seconds left: ${secondsLeft}`
+            case Status.Paused:  return "[PAUSED]"
+            case Status.Lost:    return ":("
+            case Status.Won:     return ":("
+            case Status.Stopped: return "^_^"
+            default:             throw Error(`invalid status ${status}`)
+          }
+        })()
+      }
+    </>
+  }
+
+  return <>
+    <div className="status-line">
+      <LeftSide status={status} input={input}/>
+      <RightSide status={status} secondsLeft={secondsLeft}/>
+    </div>
+    <style jsx>{`
+      .status-line {
+        color: gray;
+        display: flex;
+        justify-content: space-between;
+        font-size: 1.5rem;
+      }
+    `}</style>
+  </>
+}
